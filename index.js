c58645020b435cf73135f3852dc18fc1de696789
berduc <bernard_ducharme@yahoo.ca>
2021-06-06 12:28:30 -0400

    ajout des modals, accueil et fin de partie

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 let score; 
 let reponses = 0;
 let caseEnCours; 
+let temps = 0; 
+let chrono; 
+let temps_debut = 10;
 
 window.onload = initialisation
 
+set_bulma_modal("modalFinPartie", [], ["modalFinPartie_btn_close", "modalFinPartie_background", "modalFinPartie_btn"])
+set_bulma_modal("modalAccueil", [], ["modalAccueil_btn_close", "modalAccueil_background", "modalAccueil_btn_demarrer"])
+
 function initialisation(){
+    document.getElementById("modalAccueil").classList.add("is-active");
+}
+
+function demarrerPartie(){
+    setChrono(temps_debut);
     nouvelleCase();
     score = 0;  
     document.getElementById("score").innerHTML = "Score: 0";
+}
+
+function setChrono(secondes){
+    temps = secondes; 
+    document.getElementById("chrono").innerHTML = temps; 
+    chrono = window.setInterval(() =>{
+        temps--; 
+        document.getElementById("chrono").innerHTML = temps; 
+        if (temps === 0){
+            stopJeu();
+            annoncerResutat();
+            window.clearInterval(chrono); 
+        }
+    }, 1000)
+}
+
+function stopJeu(){}
+
+function annoncerResutat(){
+    document.getElementById("modalFinPartie").classList.add("is-active");
+    document.getElementById("txt_score_final").innerHTML = "Score final: "+score; 
 }
 
 function corrigerCouleur(couleur){
@@ -55,4 +87,19 @@ function nouvelleCase(){
     document.getElementById("laCase").innerHTML = caseEnCours.coord
     enableButtons("btnLocal");
     enableButtons("btnCouleur"); 
+}
+
+function set_bulma_modal(id_modal, openers_elements_ids, closers_elements_ids){
+    let modal = document.getElementById(id_modal);
+    openers_elements_ids.forEach(element =>
+        document.getElementById(element).addEventListener('click', () => {
+            modal.classList.add("is-active");
+        })
+    );
+    
+    closers_elements_ids.forEach(element =>
+        document.getElementById(element).addEventListener('click', () => {
+            modal.classList.remove("is-active");		
+        })
+    );
 }
